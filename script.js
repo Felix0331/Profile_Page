2e897962eb882168a1a2205ddee0f599feb2d77b
Felix Almejo <fd.almejo@gmail.com>
2021-07-26 16:52:33 -0700

    Added functionality to request accept and decline buttons

---
+++ script.js
@@ -0,0 +1,25 @@
+function changeName(id){
+    document.getElementById(id).innerText="Jane Doe";
+}
+
+//adding and removing people from requests
+
+var pendingRequestNum = 2;
+var currentConnections = 450;
+document.getElementById("connect-number").innerText = currentConnections;
+document.getElementById("req-number").innerText=pendingRequestNum;
+
+function increaseFriends(request){
+    pendingRequestNum--;
+    currentConnections++;
+    document.getElementById("connect-number").innerText = currentConnections;
+    document.getElementById("req-number").innerText=pendingRequestNum;  
+    document.getElementById(request).remove();
+}
+
+function keepFriendsSame(request){
+    document.getElementById(request).remove();
+    pendingRequestNum--;
+    document.getElementById("req-number").innerText=pendingRequestNum;  
+
+}
